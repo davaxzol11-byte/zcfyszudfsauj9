6e63c32dbc05e547b0549b78d3fb4a40c7aa5ace
davaxzol11-byte <davaxzol11@gmail.com>
2026-03-26 19:42:27 +0300

    Add files via upload

--- start-all.js
+++ start-all.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
 
-// Запускаем бот
-require('./bot.js');
+// Запускаем бот напрямую (чтобы polling работал)
+const { spawn } = require('child_process');
+const botProcess = spawn('node', ['bot.js'], { stdio: 'inherit' });
 
-// Запускаем веб-сервер
-require('./server.js');
-
-console.log('✅ Все сервисы запущены!');
+// Даем боту время запуститься перед стартом сервера
+setTimeout(() => {
+    require('./server.js');
+    console.log('✅ Все сервисы запущены!');
+}, 1000);
